17a1ecdb7dbc8a3c0f394fda0550796f7831bd38
Andrew Stanton <up683693@myport.ac.uk>
2020-04-23 21:07:12 +0100

    Get Bearer Token from twitter

--- src/bearerToken.service.ts
+++ src/bearerToken.service.ts
@@ -4,6 +4,8 @@ import {
   BeforeApplicationShutdown,
 } from '@nestjs/common';
 
+import fetch from 'node-fetch';
+
 @Injectable()
 export class BearerTokenService
   implements OnApplicationBootstrap, BeforeApplicationShutdown {
@@ -16,6 +18,27 @@ export class BearerTokenService
   onApplicationBootstrap(): string {
     console.log('This works');
     this._bearerToken = 'A value';
+    const credentials = `${process.env.API_KEY}:${process.env.API_SECRET}`;
+    const encodedCredentials = Buffer.from(credentials, 'utf8').toString(
+      'base64',
+    );
+
+    fetch('https://api.twitter.com/oauth2/token', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Basic ' + encodedCredentials,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+      body: 'grant_type=client_credentials',
+    })
+      .then(res => res.json()) // expecting a json response
+      .then(json => {
+        console.log(json);
+        if (json.token_type === 'bearer') {
+          this._bearerToken = json.access_token;
+          console.log('Bearer token received');
+        }
+      });
     return 'Hello World!';
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config();
 
 declare const module: any;
 
