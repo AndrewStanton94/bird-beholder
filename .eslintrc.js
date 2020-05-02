840a86960d5588717c801f3fbff834126bea86fe
Andrew Stanton <up683693@myport.ac.uk>
2020-05-02 22:28:45 +0100

    eslist update
    - Turned off camelcase (twitter variables)
    - Removed deprecated rule
    - Enforcing brackets around arrow function vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,9 @@ module.exports = {
     jest: true,
   },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    'arrow-parens': 2,
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/camelcase': 'off',
   },
 };
